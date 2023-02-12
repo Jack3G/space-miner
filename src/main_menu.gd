0708063fb5102d3a330f0cb49583633c88eef38a
Jack3G <40290244+Jack3G@users.noreply.github.com>
2023-02-12 20:43:30 +1100

    change the menu screen spaceman to bounce around

--- src/main_menu.gd
+++ src/main_menu.gd
@@ -1,12 +1,12 @@
 extends Control
 
 @export var background_scroll: Vector2 = Vector2(30, 20)
+@export var spaceman_velocity: Vector2 = Vector2(20, 30)
 
 @onready var background: ParallaxBackground = $ParallaxBackground
 @onready var title: Label = %Title
 @onready var play_button: Button = %PlayButton
-@onready var spaceman: Sprite2D = %FloatingSpaceMan
-@onready var animation: AnimationPlayer = $AnimationPlayer
+@onready var spaceman: CharacterBody2D = %FloatingSpaceMan
 @onready var play_pressed: AudioStreamPlayer = $PlayPressed
 
 
@@ -21,11 +21,16 @@ func _ready() -> void:
 		get_tree().get_root().add_child(load("res://src/game.tscn").instantiate())
 		self.queue_free())
 
+	spaceman.velocity = spaceman_velocity
+
 	title.visible_ratio = 0
 	await self.create_tween().tween_property(title, "visible_ratio", 1, 3).finished
-
-	animation.current_animation = "he orbit"
 
 func _process(delta: float) -> void:
 	background.scroll_offset += background_scroll * delta
 	spaceman.rotation -= PI/2 * delta
+
+func _physics_process(delta: float) -> void:
+	if spaceman.move_and_slide():
+		var collision = spaceman.get_last_slide_collision()
+		spaceman.velocity = spaceman.velocity.bounce(collision.get_normal())
