ba52f3e2a8d656b54f16957b7174934b984453b0
Jack3G <40290244+Jack3G@users.noreply.github.com>
2023-02-12 21:00:37 +1100

    add a highscore tracker
    There's nowhere to view it yet.

--- src/game_over.gd
+++ src/game_over.gd
@@ -9,11 +9,14 @@ var _characters_in_duration: int = 0
 # we need these because load_ui_package is called before the node is ready
 var _gold_text: String = ""
 var _duration_text: String = ""
+var _new_high_score: bool = false
 
 @onready var gold: Label = %Gold
 @onready var duration: Label = %Duration
 @onready var menu_button: Button = %MenuButton
 @onready var text_scroll: AudioStreamPlayer = $TextScroll
+@onready var highscore: Control = %Highscore
+@onready var highscore_sound: AudioStreamPlayer = $HighscoreSound
 
 
 func load_ui_package(package: Dictionary) -> void:
@@ -36,6 +39,24 @@ func load_ui_package(package: Dictionary) -> void:
 
 		_duration_text = duration_prefix + str(time)
 		_characters_in_duration = str(time).length()
+
+	var save_file = ConfigFile.new()
+	var err = save_file.load("user://save.ini")
+	if err != OK:
+		print("Couldn't load the save file. Creating a new one.")
+		save_file.set_value("highscore", "gold", -1)
+		save_file.set_value("highscore", "duration", -1)
+
+	if save_file.has_section_key("highscore", "gold") and package.has("gold"):
+		if package.gold >= save_file.get_value("highscore", "gold"):
+			save_file.set_value("highscore", "gold", package.gold)
+			_new_high_score = true
+	if save_file.has_section_key("highscore", "duration") and package.has("game_time"):
+		if package.game_time >= save_file.get_value("highscore", "duration"):
+			save_file.set_value("highscore", "duration", package.game_time)
+			_new_high_score = true
+
+	save_file.save("user://save.ini")
 
 func _ready() -> void:
 	menu_button.pressed.connect(func():
@@ -73,3 +94,12 @@ func _ready() -> void:
 		duration, "visible_characters",
 		duration.text.length(), 1).finished
 	await get_tree().create_timer(1).timeout
+
+	if _new_high_score:
+		for i in range(3):
+			highscore.visible_characters = -1
+			highscore_sound.play()
+			await get_tree().create_timer(0.5).timeout
+			highscore.visible_characters = 1
+			await get_tree().create_timer(0.5).timeout
+		highscore.visible_characters = -1
